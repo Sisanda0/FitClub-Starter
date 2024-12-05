3b9ad04ce6e74a748b0339863708537772ce3c18
sisanda0 <s.manqina@calivision.tech>
2024-12-06 00:40:26 +0200

    Hover orange

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -8,7 +8,7 @@ const Header = () => {
 
             <img src={Logo} alt="" className='logo' />
 
-            <ul>
+            <ul className='header-menu'>
                 <li>Home</li>
                 <li>Programs</li>
                 <li>Why us</li>
