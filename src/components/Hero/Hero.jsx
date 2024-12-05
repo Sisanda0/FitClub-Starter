9b0668407ecc37c67e6fa6c1d7f2f9a02f6de57f
sisanda0 <s.manqina@calivision.tech>
2024-12-06 01:58:30 +0200

    Message added

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -16,11 +16,17 @@ const Hero = () => {
             {/* Hero Heading*/}
             <div className="hero-text">
                 <div>
-                    <span>Shape </span>
+                    <span className='stroke-text'>Shape </span>
                     <span>Your</span>
                 </div>
                 <div>
-                    <span>Ideal body</span>
+                 <span>Ideal body</span>
+                </div>
+                <div>
+                    <span>
+                      In here we will help you to shape and build your ideal body and 
+                      live up your life to fullest 
+                    </span>
                 </div>
             </div>
         </div>
