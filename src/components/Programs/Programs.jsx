8854f5faad1db2f126504ff65ca37b7646b562db
sisanda0 <s.manqina@calivision.tech>
2024-12-06 15:37:35 +0200

    Added program data

--- src/components/Programs/Programs.jsx
+++ src/components/Programs/Programs.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import './Programs.css'
+import {programsData} from '../../data/programsData'
 const Programs = () =>{
     return (
     <div className='Programs' id='programs'>
@@ -9,10 +10,24 @@ const Programs = () =>{
             <span>Programs</span>
             <span className='stroke-text'>to shape you</span>
 
+        </div>
+        <div className="program-categories">
+            {
+                programsData.map((program)=>(
+                    <div className='category'>
+                        {program.image}
+                        <span>{program.heading}</span>
+                        <span>{program.details}</span>
 
+
+                    </div>
+                )
+
+                )
+            }
         </div>
     </div>
-    )
-}
+    );
+};
 
 export default Programs
